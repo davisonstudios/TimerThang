222731b712deecc8e7e4c53678985ece2ebe997c
Stuart Davison <stuart@davisonstudios.com>
2021-08-16 10:12:54 -0700

    Initial Commit

---
+++ TimerThang/TimerThangApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerThangApp.swift
+//  TimerThang
+//
+//  Created by Stuart Davison on 8/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerThangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
